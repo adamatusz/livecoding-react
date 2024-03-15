441fa9fe161328e0b609c5460f6e215f03368226
Grzegorz Wieraszka <gerwazy911@gmail.com>
2024-03-15 14:23:26 +0100

    code completion

--- react-with-devstock/src/App.jsx
+++ react-with-devstock/src/App.jsx
@@ -1,28 +1,36 @@
-import UserCard from './components/UserCard'
+// function CustomComponent(( text = 'Dołącz do Devstock' )) {
+//  return <h2 style={{color: 'blue', padding: '20px'}}>{text}</h2>
+// }
 
-const user = {
-  name: 'Jan Kowalski',
-  nickname: 'jankowalski',
-  email: 'jan.kowalski@example.com',
-  phone: '123-456-789',
-}
-
-const secondUser = {
-  name: 'Anna Nowak',
-  nickname: 'annanowak',
-  email: 'anna.nowak@example.com',
-}
+import UserCard from "./components/UserCard";
 
 function App() {
-  const isLogged = false
+  const user = {
+    id: 1,
+    name: "Jan Kowalski",
+    nickname: "jankowalski",
+    // phone:"123-456-789",
+    isLoggedIn: false,
+  };
+  const user2 = {
+    id: 2,
+    name: "Anna Kowalska",
+    nickname: "annakowalska",
+    email: "anna.kowalska@example.com",
+    phone: "555-456-789",
+    isLoggedIn: true,
+  };
 
+  const users = [user, user2];
   return (
-    <>
-      <UserCard user={user} isLogged />
-      <hr />
-      <UserCard user={secondUser} isLogged={isLogged} />
-    </>
-  )
+    <div>
+      {users
+        .filter((user) => user.isLoggedIn)
+        .map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- react-with-devstock/src/components/UserCard.jsx
+++ react-with-devstock/src/components/UserCard.jsx
@@ -1,16 +1,20 @@
-export default function UserCard({ user, isLogged }) {
+const UserCard = ({ user }) => {
+  if (!user.isLoggedIn) {
+    return null;
+  }
+
   return (
     <div>
       <p>Name: {user.name}</p>
       <p>Nickname: {user.nickname}</p>
-      {isLogged ? (
-        <>
-          <p>Email: {user.email}</p>
-          {user.phone && <p>Phone: {user.phone}</p>}
-        </>
+      <p>Email: {user.email}</p>
+      {user.phone ? (
+        <p>Phone:{user.phone}</p>
       ) : (
-        <p>Please log in to see contact details.</p>
+        <p>{user.name} nie ma telefonu</p>
       )}
     </div>
-  )
-}
+  );
+};
+
+export default UserCard;
