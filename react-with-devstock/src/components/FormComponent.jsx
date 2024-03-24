a3480ada39edede1b73f3f0389574c693de84724
Bartlomiej Loza <bekloza-contractor@endeavorco.com>
2024-03-25 00:30:50 +0100

    after livecoding

--- react-with-devstock/src/components/FormComponent.jsx
+++ react-with-devstock/src/components/FormComponent.jsx
@@ -1,46 +1,72 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+const soupSchema = z.object({
+  title: z.string().min(1, { message: "Required" }),
+  description: z.string().min(10, { message: "Too short" }),
+});
 
 function FormComponent({ setSoup }) {
-  const [title, setTitle] = useState('Pomidorowa')
-  const [description, setDescription] = useState('')
-  const handleSubmit = (event) => {
-    console.log('title', title)
-    console.log('description', description)
-    event.preventDefault();
-    setSoup({
-      title,
-      description,
-      ingredients: {
-        one: 'sczypta soli',
-        two: 'szklanka mleka',
-        three: 'kawałek pomidora'
-      }
-    })
-  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    watch,
+  } = useForm({
+    resolver: zodResolver(soupSchema),
+    defaultValues: {
+      title: "Pomidorowa",
+      description: "pyszna zupa",
+    },
+  });
 
-  const onTitleChangeHandler = (event) => {
-    setTitle(event.target.value)
-  }
+  console.log("errors", errors);
+  console.log(watch("title"));
+  const onSubmit = (data) => {
+    console.log("data", data);
 
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(true);
+      }, 10 * 1000);
+    });
+  };
 
-
-  return <div>
-    <form onSubmit={handleSubmit}>
-      <div>
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label htmlFor="title">Tytuł</label>
+          <div>
+            <label htmlFor="title">Tytuł</label>
+          </div>
+          <input
+            {...register("title")}
+            id="title"
+            name="title"
+            type="text"
+            placeholder="Tytuł"
+          />
+          {errors?.title && <p>{errors.title.message}</p>}
         </div>
-        <input value={title} id="title" name="title" type="text" placeholder="Tytuł" onChange={onTitleChangeHandler} />
-      </div>
-      <div>
         <div>
-          <label htmlFor="description">Opis</label>
+          <div>
+            <label htmlFor="description">Opis</label>
+          </div>
+          <textarea
+            {...register("description")}
+            id="description"
+            name="description"
+            placeholder="Opis"
+            rows={4}
+          />
         </div>
-        <textarea id="description" value={description} name="description" placeholder="Opis" rows={4} onChange={(event) => setDescription(event.target.value)} />
-      </div>
-      <button type="submit">Wyślij</button>
-    </form>
-  </div>
+        <button disabled={isSubmitting} type="submit">
+          {isSubmitting ? "..." : "Wyślij"}
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default FormComponent
+export default FormComponent;
