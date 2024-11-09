cfb39a0226c01bbf9ee1a8fadc9a723057b9fff6
adamatusz <adamatusz@gmail.com>
2024-11-09 11:10:27 +0100

    livecodingEnd

--- react-with-devstock/src/App.jsx
+++ react-with-devstock/src/App.jsx
@@ -1,19 +1,45 @@
-import { useState } from 'react'
-import Soup from './components/Soup'
-import Button from './components/Button'
-import FormComponent from './components/FormComponent'
+import { useState } from "react";
+import Soup from "./components/Soup";
+import Button from "./components/Button";
+import FormComponent from "./components/FormComponent";
+
+const defaultSoup = {
+  title: "Zupa Ogórkowa",
+  description:
+    "Zupa ogórkowa jest jednym z najbardziej charakterystycznych dań kuchni polskiej.",
+  ingredients: {
+    one: "litr bulionu drobiowego lub warzywnego",
+    two: "dużych kiszonych ogórków",
+    three: "duża marchewka",
+  },
+};
 
 function App() {
-  const [hideButtons, setHideButtons] = useState(false)
+  const [hideButtons, setHideButtons] = useState(false);
+  const [soup, setSoup] = useState(null);
   return (
     <>
       <Button onClick={() => setHideButtons((prev) => !prev)}>
-        {hideButtons ? 'Pokaż' : 'Ukryj'} przyciski akcji
+        {hideButtons ? "Pokaż" : "Ukryj"} przyciski akcji
       </Button>
-      <Soup hideButtons={hideButtons} />
-      <FormComponent />
+      <Soup
+        hideButtons={hideButtons}
+        title={defaultSoup.title}
+        description={defaultSoup.description}
+        ingredients={defaultSoup.ingredients}
+        recipe={defaultSoup.ingredients}
+      />
+      {soup && (
+        <Soup
+          hideButtons={hideButtons}
+          title={soup.title}
+          description={soup.description}
+          recipe={soup.ingredients}
+        />
+      )}
+      <FormComponent setSoup={setSoup} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- react-with-devstock/src/components/FormComponent.jsx
+++ react-with-devstock/src/components/FormComponent.jsx
@@ -1,5 +1,69 @@
-function FormComponent() {
-  return <div></div>
+// import { useRef } from "react";
+
+import { useState } from "react";
+
+function FormComponent({ setSoup }) {
+  const [title, setTitle] = useState("Pomidorowa");
+  const [description, setDescription] = useState("");
+  // const titleRef = useRef(null);
+  const handleSubmit = (event) => {
+    console.log("title", title);
+    console.log("description", description);
+    // const formData = new FormData(event.target);
+    event.preventDefault();
+    setSoup({
+      title,
+      description,
+      ingredients: {
+        one: "szczypta soli",
+        two: "pół szklanki śmietany",
+        three: "kawałek pomidora",
+      },
+    });
+    // console.log(formData.get('title'))
+    // console.log(titleRef.current.value);
+  };
+
+  const onTitleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
+  console.log("title", title);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>
+            <label htmlFor="title">Tytuł</label>
+          </div>
+          <input
+            value={title}
+            // ref={titleRef}
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Tytuł"
+            onChange={onTitleChangeHandler}
+          />
+        </div>
+        <div>
+          <div>
+            <label htmlFor="description">Opis</label>
+          </div>
+          <textarea
+            name="description"
+            id="description"
+            value={description}
+            placeholder="Opis"
+            rows={4}
+            onChange={(event) => setDescription(event.target.value)}
+          />
+        </div>
+        <button type="submit">Wyślij</button>
+      </form>
+    </div>
+  );
 }
 
-export default FormComponent
+export default FormComponent;
